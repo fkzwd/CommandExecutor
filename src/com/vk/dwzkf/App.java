f1bcf19a70ade4ac00aa148eb1a61896424bce34
fkzwd <lives352@gmail.com>
2020-08-31 01:23:07 +0300

    Add psvm starting server

--- src/com/vk/dwzkf/App.java
+++ src/com/vk/dwzkf/App.java
@@ -1,7 +1,11 @@
 package com.vk.dwzkf;
 
+import com.vk.dwzkf.server.Server;
+
 public class App {
     public static void main(String[] args) {
-
+        Server server = new Server();
+        server.setUp();
+        server.start();
     }
 }
